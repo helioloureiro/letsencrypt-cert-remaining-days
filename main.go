b051c82224c2721f4045484bfb586d9ce067f1f8
Helio Loureiro <helio.loureiro@xensam.com>
2025-04-17 14:26:10 +0200

    Some basic functionality and testing

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
-	"os"
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strings"
 )
 
-var Version = "development"
+const (
+	LETSENCRYPTDIR = "/etc/letsencrypt"
+)
 
+var (
+	Version         = "development"
+	CheckAllDomains = false
+)
 
 func main() {
 	version := flag.Bool("version", false, "version")
+	letsencryptdir := flag.String("letsencryptdir", LETSENCRYPTDIR, "Root directory for lestencrypt installation")
+	domain := flag.String("domain", "", "Domain to be checked (otherwise all domains will be checked)")
 	flag.Parse()
 
 	if *version {
@@ -18,4 +30,72 @@ func main() {
 		os.Exit(0)
 	}
 
+	if *letsencryptdir != LETSENCRYPTDIR {
+		fmt.Println("Using directory:", *letsencryptdir)
+	}
+
+	if *domain == "" {
+		fmt.Println("Checking all domains.")
+		CheckAllDomains = true
+	}
+
+	if CheckAllDomains {
+		configFiles := getListOfConfigs(fmt.Sprintf("%s/renewal", *letsencryptdir))
+		fmt.Println("configFiles:", configFiles)
+	} else {
+		configFile := fmt.Sprintf("%s/renewal/%s.conf", *letsencryptdir, *domain)
+		certificate := getCertificatePathFromConfig(configFile)
+		fmt.Println("certificate:", certificate)
+
+	}
+
+}
+
+func getListOfConfigs(directory string) []string {
+	files, err := os.ReadDir(directory)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to read from directory: %v", directory))
+	}
+
+	var configFiles []string
+	for _, file := range files {
+		if grep(".conf", file.Name()) {
+			configFiles = append(configFiles, file.Name())
+		}
+	}
+	return configFiles
+}
+
+func grep(pattern, text string) bool {
+	re := regexp.MustCompile(pattern)
+	return re.MatchString(text)
+}
+
+func getCertificatePathFromConfig(configFile string) string {
+	file, err := os.Open(configFile)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to open file: %v", configFile))
+	}
+	defer file.Close()
+
+	var certificate = ""
+	rd := bufio.NewReader(file)
+	for {
+		line, _, err := rd.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+		}
+		strLine := string(line)
+		if grep("^cert", strLine) {
+			strLine = sed(" ", "", strLine)
+			certificate = strings.Split(strLine, "=")[1]
+		}
+	}
+	return certificate
+}
+
+func sed(before, after, text string) string {
+	return strings.Replace(text, before, after, -1)
 }
---
+++ main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+)
+
+
+func TestGrep(t *testing.T) {
+	result := grep("helio", "helio loureiro")
+	if result != true {
+		t.Errorf("grep failed to find the parttern")
+	}
+
+	result = grep("something", "helio loureiro")
+	if result != false {
+		t.Errorf("grep failed and found a pattern")
+	}
+}
+
+
+func TestSed(t *testing.T) {
+	text := "helio loureiro"
+	expected := "helioloureiro"
+	response := sed(" ", "", text)
+	if response != expected {
+		t.Errorf("Expected: %s - got: %s", expected, response)
+	}
+
+	expected = "HeLio Loureiro"
+	response = sed("h", "H", text)
+	response = sed("l", "L", response)
+	if response != expected {
+		t.Errorf("Expected: %s - got: %s", expected, response)
+	}
+}
